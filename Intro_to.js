11a618c79c34213c65f9eee85c6a7b92aece0089
Aziza Ayoub <meaziza0@gmail.com>
2023-08-09 21:31:39 +0430

    add to file and done two exercises.

---
+++ Intro_to.js
@@ -0,0 +1,39 @@
+// The console is a panel that displays important messages, like errors, for developers.
+
+// One action, or method, that is built into the console object is the .log() method.
+
+console.log('My name is Aziza'); // My name is Aziza
+console.log(19); // output 19
+console.log(15); // output 15
+
+
+
+// Comments
+
+// Comments can explain what the code is doing, leave instructions for developers using the code, or add any other useful annotations.
+
+// There are two types of code comments in JavaScript:
+
+// A single line comment will comment out a single line and is denoted with two forward slashes // preceding it.
+
+// like this
+// Prints 5 to the console
+// console.log(5);
+ 
+ 
+// A multi-line comment will comment out multiple lines and is denoted with /* to begin the comment, and */ to end the comment.like 
+
+
+
+/*
+This is all commented 
+console.log(10);
+None of this is going to run!
+console.log(99);
+*/
+
+
+// You can also use this syntax to comment something out in the middle of a line of code:
+
+console.log(/*IGNORED!*/ 5);  // Still just prints 5
+
