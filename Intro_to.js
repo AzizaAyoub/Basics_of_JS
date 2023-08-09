6e33513259d996d1363497c943264889b48e6b57
Aziza Ayoub <meaziza0@gmail.com>
2023-08-09 21:56:48 +0430

    add a new exercise

--- Intro_to.js
+++ Intro_to.js
@@ -6,24 +6,17 @@ console.log('My name is Aziza'); // My name is Aziza
 console.log(19); // output 19
 console.log(15); // output 15
 
-
-
 // Comments
 
 // Comments can explain what the code is doing, leave instructions for developers using the code, or add any other useful annotations.
-
 // There are two types of code comments in JavaScript:
-
 // A single line comment will comment out a single line and is denoted with two forward slashes // preceding it.
 
 // like this
 // Prints 5 to the console
-// console.log(5);
- 
- 
+console.log(5);
+  
 // A multi-line comment will comment out multiple lines and is denoted with /* to begin the comment, and */ to end the comment.like 
-
-
 
 /*
 This is all commented 
@@ -32,8 +25,49 @@ None of this is going to run!
 console.log(99);
 */
 
-
 // You can also use this syntax to comment something out in the middle of a line of code:
 
 console.log(/*IGNORED!*/ 5);  // Still just prints 5
+
+// Data Types
+
+// In JavaScript, there are seven fundamental data types:
+// Number: Any number, including numbers with decimals: 4, 8, 1516, 23.42.
+// String: Any grouping of characters on your keyboard (letters, numbers, spaces, symbols, surrounded by single quotes: ' ... ' or double quotes " ... "
+// Boolean: This data type only has two possible values— either true or false (without quotes).
+// Null: This data type represents the intentional absence of a value, and is represented by the keyword null (without quotes).
+// Undefined: This data type is denoted by the keyword undefined (without quotes).
+// Symbol: A newer feature to the language, symbols are unique identifiers, useful in more complex coding.
+// Object: Collections of related data.
+// The first 6 of those types are considered primitive data types.
+
+
+onsole.log('JavaScript'); // output JavaScript
+console.log(2011); // output 2011
+console.log('Woohoo! I love to code! #codecademy'); // output Woohoo! I love to code! #codecademy
+console.log(20.49); // output 20 and 49
+
+
+// Arithmetic Operators
+
+// JavaScript has several built-in arithmetic operators, that allow us to perform mathematical calculations on numbers.
+
+/*
+Add: +
+Subtract: -
+Multiply: *
+Divide: /
+Remainder: %
+*/
+
+console.log(3 + 4); // Prints 7
+console.log(5 - 1); // Prints 4
+console.log(4 * 2); // Prints 8
+console.log(9 / 3); // Prints 3
+console.log(11 % 3); // Prints 2
+console.log(12 % 3); // Prints 0
+console.log(3.5 + 19); // prints 22.5
+console.log(2023 - 1969); // prints 54
+console.log(65 / 240); // prints 0.2708333333333333
+console.log(0.2708 * 100); // prints 27.08
 
