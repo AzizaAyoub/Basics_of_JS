ba81a8ca7036ea785af9970302e74ffe275388a2
Aziza Ayoub <meaziza0@gmail.com>
2023-08-30 10:56:49 +0430

    add fifth exercise

---
+++ exercise_5.js
@@ -0,0 +1,37 @@
+// Comparison Operators
+
+// sometimes we need to use different types of operators to compare values. These operators are called comparison operators.
+
+// Less than: <
+// Greater than: >
+// Less than or equal to: <=
+// Greater than or equal to: >=
+// Is equal to: ===
+// Is not equal to: !==
+
+// Comparison operators compare the value on the left with the value on the right.
+
+10 < 12 // Evaluates to true
+
+// We can also use comparison operators on different data types like strings:
+
+'apples' === 'oranges' // false
+
+// All comparison statements evaluate to either true or false and are made up of:
+//Two values that will be compared.
+// An operator that separates the values and compares them accordingly (>, <, <=,>=,===,!==).
+
+// declear a variable
+let hungerLevel = 7;
+if (hungerLevel > 7) { // use if statement
+  console.log('Time to eat!')
+} else { // will execute else statement
+  console.log('We can eat later!');
+};
+let number = 323; // declear variable
+let string = 'computer'; // declear second variable
+if (number >= string) { // use if...else statements
+  console.log('It\'s true!');
+} else {
+  console.log('It\'s false!'); // log It's false!
+}
