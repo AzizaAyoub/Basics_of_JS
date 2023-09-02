064dd0fa0ca2609f50ada05b378a71b144975b26
Aziza Ayoub <meaziza0@gmail.com>
2023-09-02 10:15:58 +0430

    add exercise thirteenth

---
+++ exercise_13.js
@@ -0,0 +1,37 @@
+// Calling a Function
+
+// The code inside a function body runs, or executes, only when the function is called.
+// To call a function in your code, you type the function name followed by parentheses.
+
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.');
+};
+sayThanks();
+sayThanks();
+sayThanks();
+sayThanks();
+
+function sayThanks() {
+  console.log('Thank you for your purchase! We appreciate your business.');
+};
+sayThanks();
+sayThanks();
+sayThanks();
+sayThanks();
+let afghanistan = 'Kabul'
+function myCountry() {
+  switch (afghanistan) {
+    case 'laghman':
+    console.log('It\'s a beautiful place');
+    break;
+    case 'Ghazni':
+    console.log('One of historical place in afghanistan ');
+    break;
+    case 'Kabul':
+    console.log('Kabul is the capital of Afghanistan');
+    break;
+    default:
+    console.log('All of provinces of Afghanistan are beautiful')
+  }
+};
+myCountry();
