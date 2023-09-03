9160fb3ae5345e634928695a72d7d931be537454
Aziza Ayoub <meaziza0@gmail.com>
2023-09-03 11:11:08 +0430

    add exercise seventeenth

---
+++ exercise_17.js
@@ -0,0 +1,23 @@
+// Helper Functions
+
+// We can also use the return value of a function inside another function. 
+// These functions being called within another function are often referred to as helper functions.
+
+function multiplyByNineFifths(number) {
+  return number * (9/5);
+};
+
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+};
+
+getFahrenheit(15); // Returns 59
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns) * 200;
+};
+const totalCost = costOfMonitors(5, 4);
+console.log(totalCost);
