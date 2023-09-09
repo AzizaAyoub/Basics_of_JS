033498767d232aabc7636f4d6fc54e8e11dfec3e
Aziza Ayoub <meaziza0@gmail.com>
2023-09-09 10:56:46 +0430

    comlete the exercise

---
+++ exercise_27.js
@@ -0,0 +1,12 @@
+// Arrays
+// write this list in JavaScript, as an array:
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+// Arrays are JavaScript’s way of making lists. 
+// Arrays can store any data types (including strings, numbers, and booleans). Like lists, arrays are ordered, meaning each item has a numbered position.
+
+let concepts = ['creating arrays', 'array structures', 'array manipulation'];
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
