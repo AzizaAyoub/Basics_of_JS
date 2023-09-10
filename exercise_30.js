64ea785f79056c144653468ff7c2be9663d0b5d1
Aziza Ayoub <meaziza0@gmail.com>
2023-09-10 11:20:41 +0430

    add the exercise

---
+++ exercise_30.js
@@ -0,0 +1,18 @@
+// Update Elements
+
+// Once you have access to an element in an array, you can update its value.
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+
+seasons[3] = 'Autumn';
+console.log(seasons); 
+//Output: ['Winter', 'Spring', 'Summer', 'Autumn']
+// The line, seasons[3] = 'Autumn'; tells our program to change the item at index 3 of the seasons array to be 
+// 'Autumn' instead of what is already there.
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados';
+console.log(groceryList); 
+// Output:
+[ 'bread', 'avocados', 'milk' ]
+
