93a331517299c923d9610d169878d9746c27b742
Aziza Ayoub <meaziza0@gmail.com>
2023-09-12 11:12:35 +0430

    repeating tasks manually exercise

---
+++ exercise_39.js
@@ -0,0 +1,12 @@
+// Repeating Tasks Manually
+
+// Write your code below
+let vacationSpots = ['Dubi', 'China', 'Turkey'];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+
+
+
+
