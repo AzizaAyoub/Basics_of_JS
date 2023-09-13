274cd28c0cc54c7672fafa2f081913658ab6aa01
Aziza Ayoub <meaziza0@gmail.com>
2023-09-13 12:06:04 +0430

    add another exercise

--- exercise_45.js
+++ exercise_45.js
@@ -19,3 +19,16 @@ console.log('Orange you glad I broke out the loop!');
 //Orange you glad I broke out the loop!
 
 // break statements can be especially helpful when we’re looping through large data structures!
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++){
+  console.log(rapperArray[i])
+  if (rapperArray[i] === 'Notorious B.I.G.'){
+    break;
+}
+}
+console.log("And if you don't know, now you know.");
+
+
