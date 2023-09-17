8b31ed4f3a212e733d408a6d2564503637e5948a
Aziza Ayoub <meaziza0@gmail.com>
2023-09-17 11:51:26 +0430

    adding some code

---
+++ exercise_52.js
@@ -0,0 +1,33 @@
+// The .reduce() Method
+
+// Another widely used iteration method is .reduce(). 
+// The .reduce() method returns a single value after iterating through the elements of an array, thereby reducing the array. 
+
+const numbers = [1, 2, 4, 10];
+
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+})
+
+console.log(summedNums) // Output: 17
+
+// The .reduce() method can also take an optional second parameter to set an initial value for accumulator (remember, the first argument is the callback function!).
+
+const numbers = [1, 2, 4, 10];
+
+const summedNums = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+}, 100)  // <- Second argument for .reduce()
+
+console.log(summedNums); // Output: 117
+
+const newNumbers = [1, 3, 5, 7];
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10)
+
+console.log(newSum);
+
+
