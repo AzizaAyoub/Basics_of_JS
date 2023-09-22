ca3d43dd92ea5f2da75f668f4597b54a3fb364d3
Aziza Ayoub <meaziza0@gmail.com>
2023-09-22 17:10:29 +0430

    add some changes

--- js100_exercise/eight.js
+++
@@ -1,7 +0,0 @@
-// Create an array named names that contains a list of pet names. For instance:
-
-let name = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
-
-// You can use that data or make up your own.
-
-
